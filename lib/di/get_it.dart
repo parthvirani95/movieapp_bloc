5ad2f2e17271f9ee7abf786570d3ac6136d2bdf1
Parth Virani <parthvirani95@gmail.com>
2022-01-07 23:54:17 +0530

    Article 4 Finish Dependency Injections

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
+import 'package:movieapp_bloc/data/core/api_client.dart';
 import 'package:movieapp_bloc/data/data_sources/movie_remote_data_source.dart';
 import 'package:movieapp_bloc/domain/repositories/movie_repository.dart';
 import 'package:movieapp_bloc/domain/repositories/movie_repository_impl.dart';
@@ -12,15 +13,22 @@ final getItInstance = GetIt.I;
 
 Future init() async {
   getItInstance.registerLazySingleton<Client>(() => Client());
+
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+
   getItInstance.registerLazySingleton<MovieRemoteDataSource>(
       () => MovieRemoteDataSourceImpl(getItInstance()));
 
   getItInstance
       .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+
   getItInstance
       .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+
   getItInstance.registerLazySingleton<GetPlayingNow>(
       () => GetPlayingNow(getItInstance()));
+
   getItInstance.registerLazySingleton<GetComingSoon>(
       () => GetComingSoon(getItInstance()));
 
